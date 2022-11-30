aac2fa7cbd34fa0e152f8a20ab9d994761c782b2
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2022-11-30 10:35:52 +0800

    initial commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+// ? EXERCISE 1: INTRODUCTION TO CLASSES AND OBJECTS
+
+// Programming Technique II
+
+// Member 1's Name: _____________
+// Member 2's Name: _____________
+//
+
+// ? Notes: Choose the debug mode "Console Program" to run the program.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Enter the following data: " << endl;
+	cout << "  Subject name => ";
+	cout << endl;
+
+	cout << "  Subject code => ";
+	cout << endl;
+
+	cout << "  Score earned => ";
+	cout << endl;
+
+	cout << endl
+		 << endl;
+
+	cout << "THE RESULT" << endl
+		 << endl;
+
+	cout << "Subject Code : " << endl;
+	cout << "Subject Name : " << endl;
+	cout << "Credit Hour  : " << endl;
+	cout << "Score Earned : " << endl;
+	cout << "Grade Earned : " << endl;
+	cout << "Grade Point  : " << endl;
+	cout << "Point Earned : " << endl;
+	cout << endl;
+
+	system("pause");
+
+	return 0;
+}
